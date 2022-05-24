4fe3a954e0ad5b63c8d09dd52f574d6291293c45
Pangloss1107 <97195535+Pangloss1107@users.noreply.github.com>
2022-05-23 23:12:04 -0300

    Puzzle class

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -1,10 +1,21 @@
 #Pablo 
+from numpy import empty
+import random
+
 class Puzzle:
     def __init__(self):
         # atribute1, will create a list that will contain between 5 and 10 random words
+        self.words = ["hello", "uruguay", "ecuador", 
+                "argentina", "chocolate", "milk" ]
 
         #atribute2 empty string that will be updated by random 
-        #     
-    #def choose_word(self):
-        # atribute2 = random.choose(atributo1)
+        self.secret_word  = ""
+    def choose_word(self):
+        self.secret_word = random.choice(self.words)
+        return self.secret_word
+
+    
         
+
+
+      
